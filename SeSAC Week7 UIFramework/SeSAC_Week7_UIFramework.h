a54e2a031bec90d159f234ba5731eedbb9c02812
roargenie <105975078+roargenie@users.noreply.github.com>
2022-08-16 09:26:45 +0900

    Initial Commit

---
+++ SeSAC Week7 UIFramework/SeSAC_Week7_UIFramework.h
@@ -0,0 +1,18 @@
+//
+//  SeSAC_Week7_UIFramework.h
+//  SeSAC Week7 UIFramework
+//
+//  Created by 이명진 on 2022/08/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SeSAC_Week7_UIFramework.
+FOUNDATION_EXPORT double SeSAC_Week7_UIFrameworkVersionNumber;
+
+//! Project version string for SeSAC_Week7_UIFramework.
+FOUNDATION_EXPORT const unsigned char SeSAC_Week7_UIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeSAC_Week7_UIFramework/PublicHeader.h>
+
+
